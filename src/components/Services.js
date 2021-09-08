8de0a09369fabd0ff5749720d1eb10035a27602a
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 19:04:31 +0430

    :zap: made services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import React from "react";
+import styled from "styled-components";
+import { services } from "../utils/constants";
 
 const Services = () => {
-  return <h4>services </h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br /> built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dignissimos architecto fuga dicta cum accusamus, asperiores at neque
+            voluptates eaque rem!
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map(({ id, icon, title, text }) => {
+            return (
+              <article key={id} className="service">
+                <span className="icon">{icon} </span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   h3,
@@ -68,5 +94,5 @@ const Wrapper = styled.section`
       transform: translateY(5rem);
     }
   }
-`
-export default Services
+`;
+export default Services;
