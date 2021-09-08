ebf39c379fd2aca07b51073796130f37bae59909
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 20:02:04 +0430

    :hammer: made featuredProducts layout

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from "react";
+import { useProductsContext } from "../context/products_context";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Error from "./Error";
+import Loading from "./Loading";
+import Product from "./Product";
 
 const FeaturedProducts = () => {
-  return <h4>featured products</h4>
-}
+  const {
+    productsLoading: loading,
+    productsError: error,
+    featuredProducts: featured,
+  } = useProductsContext();
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
+  return (
+    <Wrapper className="section">
+      <div className="title">
+        <h2>featured Products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="section-center featured">
+        {featured.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   background: var(--clr-grey-10);
@@ -31,6 +52,6 @@ const Wrapper = styled.section`
       grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
     }
   }
-`
+`;
 
-export default FeaturedProducts
+export default FeaturedProducts;
