9f07c32d3968cb9f8b1d18f72c2ba71d23286737
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 20:09:56 +0430

    :zap: made formatPrice helper function to avoid wierd decimal bugs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
