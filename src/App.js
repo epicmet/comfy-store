4e890ab39c90dadb7c3425a401a57ae094ce33fb
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 17:40:24 +0430

    :zap: made route handling for different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/about" exact>
+          <About />
+        </Route>
+
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+
+        <Route path="/products" exact>
+          <Products />
+        </Route>
+
+        <Route path="/products/:id" exact children={<SingleProduct />} />
+
+        <Route path="/checkout" exact>
+          <Checkout />
+        </Route>
+
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
