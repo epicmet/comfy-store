87281c151332ddf0aba98acbd8e1fb3f22765903
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 18:50:45 +0430

    :hammer: made basic checkout layout

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
-// extra imports
-import { useCartContext } from '../context/cart_context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { PageHero, StripeCheckout } from "../components";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <h4>checkout page</h4>
-}
-const Wrapper = styled.div``
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <h1>Checkout</h1>
+      </Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
