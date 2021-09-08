2eb009529b94c39b4cbce6e19464552698e4aeb2
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-08 18:35:04 +0430

    :hammer: made sidebar toggle functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,14 +9,17 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const isOpen = true;
+  const { closeSidebar, isSidebarOpen } = useProductsContext();
+  const isOpen = false;
 
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <aside
+        className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}
+      >
         <div className="sidebar-header">
           <img src={logo} alt="comfy store" className="logo" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -24,12 +27,16 @@ const Sidebar = () => {
           {links.map(({ id, url, text }) => {
             return (
               <li key={id}>
-                <Link to={url}>{text}</Link>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="checkout">Checkout</Link>
+            <Link to="checkout" onClick={closeSidebar}>
+              Checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
