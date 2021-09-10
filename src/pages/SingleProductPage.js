4783ccf8f9b81bf87aa384826466c1e0dfc20000
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-10 16:58:42 +0430

    :hammer: made images component

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -59,7 +59,7 @@ const SingleProductPage = () => {
           back to products
         </Link>
         <div className="product-center">
-          <ProductImages />
+          <ProductImages images={images} />
           <section className="content">
             <h2>{name}</h2>
             <Stars />
