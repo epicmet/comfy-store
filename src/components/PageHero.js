a74e1a54d3350179215d10916377827d9914eabd
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-10 18:18:28 +0430

    :bug: fixed multiple backslashes

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -7,8 +7,8 @@ const PageHero = ({ title, product }) => {
     <Wrapper>
       <div className="section-center">
         <h3>
-          <Link to="/">Home</Link> /
-          {product && <Link to="/products">Products</Link>} / {title}
+          <Link to="/">Home</Link>
+          {product && <Link to="/products">/ Products</Link>} / {title}
         </h3>
       </div>
     </Wrapper>
