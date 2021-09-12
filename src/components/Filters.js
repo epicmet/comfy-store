ad0c4a6011cca7cf2fce87214297482386c960d2
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-12 18:22:23 +0430

    :hammer: made company section

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -59,6 +59,24 @@ const Filters = () => {
                 );
               })}
             </div>
+
+            <div className="form-control">
+              <h5>Company</h5>
+              <select
+                name="company"
+                value={company}
+                onChange={updateFilters}
+                className="company"
+              >
+                {companies.map((c, index) => {
+                  return (
+                    <option key={index} value={c}>
+                      {c}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
           </div>
         </form>
       </div>
