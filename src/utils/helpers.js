16df69e756158c5aed0a81f6db0b40e02d8049b6
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-12 18:00:36 +0430

    :zap: made getUniqueValues function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,4 +5,12 @@ export const formatPrice = (price) => {
   }).format(price / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+
+  if (type === "colors") {
+    unique = unique.flat();
+  }
+
+  return ["all", ...new Set(unique)];
+};
