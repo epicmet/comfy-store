f032104bb2e01fd5a113dc008f47b13d4fdc3c83
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-12 19:03:15 +0430

    :hammer: made clearFilters button

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -140,6 +140,9 @@ const Filters = () => {
             />
           </div>
         </form>
+        <button type="button" className="clear-btn" onClick={clearFilters}>
+          clear filters
+        </button>
       </div>
     </Wrapper>
   );
