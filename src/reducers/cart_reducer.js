52426c9c6d1d3f8f480facd4155adc27f20b6763
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-13 19:36:59 +0430

    :hammer: handled adding new item and existing item to the cart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,41 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  const { type } = action;
 
-export default cart_reducer
+  if (type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((item) => item.id === id + color);
+    if (tempItem) {
+      const newCart = state.cart.map((item) => {
+        if (item.id === id + color) {
+          let newAmount = item.amount + amount;
+          if (newAmount > item.max) newAmount = item.max;
+          return { ...item, amount: newAmount };
+        } else {
+          return item;
+        }
+      });
+
+      return { ...state, cart: newCart };
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
