3f7b7f125257bd7605149abb233c29119539069b
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-13 19:47:36 +0430

    :hammer: stored cart content to localstorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -9,7 +9,7 @@ import {
 } from "../actions";
 
 const initialState = {
-  cart: [],
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
   totalItems: 0,
   totalAmount: 0,
   shippingFee: 534,
@@ -19,6 +19,10 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
