0b2afb50ac0f394592a7e12eb6e72aec33d53c22
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-13 19:23:46 +0430

    :zap: added functionality for adding items to the cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,8 @@ import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
+
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
@@ -46,7 +48,11 @@ const AddToCart = ({ product }) => {
       </div>
       <div className="btn-container">
         <AmountButtons amount={amount} changeAmount={changeAmount} />
-        <Link to="/cart" className="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
