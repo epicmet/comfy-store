673b5de98ead1a5f2b2154392094b9ec3a9491e8
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-14 17:46:51 +0430

    :bug: used useAuth0 instead of useUserContext for faster access to user info

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -4,15 +4,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 const UserContext = React.createContext();
 
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+  const { loginWithRedirect, logout, user } = useAuth0();
 
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    if (isAuthenticated) setMyUser(user);
-    else setMyUser(false);
-    // eslint-disable-next-line
-  }, [isAuthenticated]);
+    setMyUser(user);
+  }, [user]);
 
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
