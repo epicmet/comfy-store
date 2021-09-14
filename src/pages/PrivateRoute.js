f4eb260d2d1cf37761098e932ee2dc8874fd7b2d
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-14 17:50:14 +0430

    :bug: used useAuth0 instead of useUserContext for faster access to user info

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useUserContext } from "../context/user_context";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const { myUser } = useUserContext();
+  const { user } = useAuth0();
 
   return (
     <Route
       {...rest}
       render={() => {
-        return myUser ? children : <Redirect to="/" />;
+        return user ? children : <Redirect to="/" />;
       }}
     ></Route>
   );
