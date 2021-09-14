314091a02b4ca097e79b9794bac4b7472795eb10
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-09-14 16:14:37 +0430

    :zap: provided function and user info to use in other places in app

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -4,15 +4,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 const UserContext = React.createContext();
 
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
-    useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
 
   const [myUser, setMyUser] = useState(null);
 
   useEffect(() => {
-    console.log(`user: ${user}`);
-    console.log(`isAuthenticated: ${isAuthenticated}`);
-    console.log(`isLoading: ${isLoading}`);
+    if (isAuthenticated) setMyUser(user);
+    else setMyUser(false);
+    // eslint-disable-next-line
   }, [isAuthenticated]);
 
   return (
